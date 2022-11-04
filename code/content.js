0999acdba41ae96cb13c8e601ecca8b8ececdfeb
Felicia Wang <felicia.wang.08@gmail.com>
2022-11-04 15:14:20 -0500

    Add more test code

--- code/content.js
+++ code/content.js
@@ -1,1 +1,8 @@
 // BM25 code will go here
+console.log("This page runs");
+
+class BM25 {
+    constructor() {
+        // instance variables for class will go in here
+    }
+}
---
+++ code/popup/popup.js
@@ -0,0 +1,1 @@
+// Functions used in popup will go here
