21dc75a917465c5646341c3f28eb6583a659fdfe
Phyllis Wang <pjw@pjw.local>
2022-11-04 20:09:11 -0500

    added search input and display query as result in popup

--- code/popup/popup.js
+++ code/popup/popup.js
@@ -1,1 +1,9 @@
 // Functions used in popup will go here
+
+function handleSubmit(event) {
+    event.preventDefault();
+    document.getElementById("output1").textContent = document.getElementById("query").value[0];
+    document.getElementById("output2").textContent = document.getElementById("query").value.slice(1);
+}
+
+document.getElementById('form').addEventListener('submit', handleSubmit);
