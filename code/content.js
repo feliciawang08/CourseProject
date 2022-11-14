7a8f322728e3a9cdf53714f939a960fd26682660
Eshia Rustagi <efrustagi@gmail.com>
2022-11-13 21:21:29 -0600

    regex for brackets

--- code/content.js
+++ code/content.js
@@ -16,6 +16,7 @@ class BM25 {
       // stopwords obtained from https://gist.github.com/sebleier/554280 (NLTK's list of english stopwords)
       var stopwords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now'];
       var cleanedText = pageText.toLowerCase()
+                                .replace(/[\[0-9\]']+/g, "") // remove [#]
                                 .replace(/[^\w\s]/g, "") // remove punctuation
                                 .replace(/^\s+|\s+$|\s+(?=\s)/g, "") // remove whitespace
                                 .trim() // remove space from beginning and end
