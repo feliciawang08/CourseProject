b7252524571d70ed5919e21b2a857a442a61fcd3
Felicia Wang <felicia.wang.08@gmail.com>
2022-12-02 11:48:37 -0600

    Test push

--- code/popup/popup.js
+++ code/popup/popup.js
@@ -6,16 +6,51 @@ function handleSubmit(event) {
     document.getElementById("output2").textContent = document.getElementById("query").value.slice(1);
 }
 
-document.getElementById('form').addEventListener('submit', handleSubmit);
+// document.getElementById('form').addEventListener('submit', handleSubmit);
 
-const submitButton = document.getElementById("submitButton");
-submitButton.onclick = async function(e) {
-    let queryOptions = {active: true, currentWindow: true};
-    let tabs = await chrome.tabs.query(queryOptions);
+function sendQuery(event) {
+    event.preventDefault();
+    var query = document.getElementById("query").value;
+    chrome.tabs.query(
+        { currentWindow: true, active: true },
+        function (tabs) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: getParagraphs
+            }, (resp) => {
+                let paragraphs = resp[0].result;
+                console.log(query);
+                chrome.tabs.sendMessage(tabs[0].id, { "query": query, "tabs": tabs, "paragraphs": paragraphs }, function (response) {
+                    console.log(response.scores);
 
-    chrome.tabs.sendMessage(
-        tabs[0].id,
-        {query: document.getElementById("query").value}
+                    var idxs = Array.from(Array(response.scores.length).keys())
+                        .sort((a, b) => response.scores[a] < response.scores[b] ? -1 : (response.scores[b] < response.scores[a]) | 0).reverse();
+
+                    console.log(idxs);
+                    document.getElementById("output1").textContent = paragraphs[idxs[0]];
+                    document.getElementById("output2").textContent = paragraphs[idxs[1]];
+                });
+            });
+        }
     )
-    console.log("Clicked");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("submitButton").addEventListener("click", sendQuery);
+})
+
+
+/**
+ * Each paragraph is a document. (M total documents, each document is 'd')
+ */
+function getParagraphs() {
+    console.log("getParagraphs");
+    let paraCount = document.getElementsByTagName("p").length;
+    let paragraphs = []
+
+    for (let i = 0; i < paraCount; i++) {
+        paragraphs.push(document.getElementsByTagName("p")[i].innerText)
+    }
+
+    return paragraphs;
 }
